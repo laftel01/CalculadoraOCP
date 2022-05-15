82d0edf47bc038134fe32057623ac7d38472cdb9
Carlos Henrique Baby de Lima Gonçalves <laftel@gmail.com>
2022-05-15 11:16:12 -0300

    Separando switch para facilitar extensão

--- src/calculadora/Interface.java
+++ src/calculadora/Interface.java
@@ -3,40 +3,23 @@ package calculadora;
 import java.util.Scanner;
 
 public class Interface {
-	private double valor1, valor2;
+	public double valor1, valor2;
+	public String resumo;
     public void calc(){
     Scanner s = new Scanner(System.in);
         System.out.println("Digite o valor1:");
         valor1 = s.nextDouble();
-        String resumo =  "" + valor1;
+        resumo =  "" + valor1;
         System.out.println("Digite o simbolo da operacao: ");
         char operador = s.next().charAt(0);
         while(operador!='='){
-            Operacao o;
-            switch(operador){
-                case '+' -> {
-                    resumo = defOperacaoSimples(resumo,true,s,operador);
-                    o = new Soma(valor1 ,valor2);
-                }
-                case '-' -> {
-                    resumo = defOperacaoSimples(resumo,true,s,operador);
-                    o = new Subtracao(valor1 ,valor2);
-                }
-                case '*' -> {
-                    resumo = defOperacaoSimples(resumo,false,s,operador);
-                    o = new Multiplicacao(valor1 ,valor2);
-                }
-                case '/' -> {
-                    resumo = defOperacaoSimples(resumo,false,s,operador);
-                    o = new Divisao(valor1 ,valor2);
-                }
-                default -> {
-                	System.out.println("Operacao invalida.");
-                	System.out.println("Digite o simbolo da operacao: ");
-                    operador = s.next().charAt(0);
-                    continue;
-                }
-            }
+            Operacao o = switchOperacoes(operador,s);
+			if(o==null) {
+				System.out.println("Operacao invalida.");
+				System.out.println("Digite o simbolo da operacao: ");
+				operador = s.next().charAt(0);
+				continue;
+			}
             valor1 = o.executarOperacao();
             System.out.println("Digite o simbolo da operacao: ");
             operador = s.next().charAt(0);
@@ -51,4 +34,28 @@ public class Interface {
         String retorno = resumo + operador + valor2;
         return parentesis ? "(" + retorno + ")" : retorno;
     }
+    
+	public Operacao switchOperacoes(char operador, Scanner s){
+		switch(operador){
+			case '+' -> {
+				resumo = defOperacaoSimples(resumo,true,s,operador);
+				return new Soma(valor1 ,valor2);
+			}
+			case '-' -> {
+				resumo = defOperacaoSimples(resumo,true,s,operador);
+				return new Subtracao(valor1 ,valor2);
+			}
+			case '*' -> {
+				resumo = defOperacaoSimples(resumo,false,s,operador);
+				return new Multiplicacao(valor1 ,valor2);
+			}
+			case '/' -> {
+				resumo = defOperacaoSimples(resumo,false,s,operador);
+				return new Divisao(valor1 ,valor2);
+			}
+			default -> {
+				return null;
+			}
+		}
+	}
 }
